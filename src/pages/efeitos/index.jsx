2845d42a023f5147aff9776f6267a995a5c5a7ed
macedorayc <macedorayane335@gmail.com>
2024-08-25 21:19:52 -0300

    video 37 atu

--- src/pages/efeitos/index.jsx
+++ src/pages/efeitos/index.jsx
@@ -1,7 +1,8 @@
 import './index.scss';
-import { useState, useEffect } from 'react';
 
+import {tratarNumero} from '../../utils/conversao';
 import Cabecalho from '../../components/cabecalho';
+import { useState, useEffect } from 'react';
 
 
 
@@ -9,10 +10,49 @@ export default function Efeitos() {
     const [mencao, setMencao] = useState('');
     const [sitMencao, setSitMencao] = useState('-');
 
+    const [nota1, setNota1] = useState('0');
+    const [nota2, setNota2] = useState('0');
+    const [nota3, setNota3] = useState('0');
+    const [media, setMedia] = useState(0);
+    const [sitAluno, setSitAluno] = useState('-');
+
+
+
     useEffect(() => {
         avaliarMencao();
 
     }, [mencao])
+
+    useEffect(() => {
+        avaliarNotas()
+    }, [nota1, nota2, nota3])
+
+    useEffect(() => {
+        avaliarSituacao()
+    }, [media])
+
+
+
+    function avaliarSituacao() {
+        let s = '';
+        if (media >= 6) {
+            s = 'Aprovado';
+        }
+        else {
+            s = 'DP'
+        }
+
+        setSitAluno(s);
+    }
+
+
+
+
+
+
+    function avaliarNotas() {
+        let m = (tratarNumero(nota1) + tratarNumero(nota2) + tratarNumero(nota3) /3); 
+    }
 
 
     function avaliarMencao() {
@@ -53,18 +93,18 @@ export default function Efeitos() {
 
                 <div className='metas-aluno'>
                     <div className='entradas'>
-                        <input type='text' />
-                        <input type='text' />
-                        <input type='text' />
+                        <input type='text' value={nota1} onChange={e => setNota1(e.target.value)}/>
+                        <input type='text' value={nota2} onChange={e => setNota2(e.target.value)}/>
+                        <input type='text' value={nota3} onChange={e => setNota3(e.target.value)}/>
                     </div>
                     <div className='media'>
                         <label> Média: </label>
-                        <div> 0.0 </div>
+                        <div> {media.toFixed(1)} </div>
                         
                     </div>
                     <div className='media-situacao'>
                         <label> Sit.:  </label>
-                        <div> Aprovado </div>
+                        <div> {sitAluno} </div>
                     </div>
 
                 </div>
